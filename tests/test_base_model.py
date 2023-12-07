dce7f8eee71adac38fa39b496185be824f1d9d70
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-07 15:06:39 -0800

    done

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,44 @@
 #!/usr/bin/python3
+"""Module for testing BaseModel class."""
+import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(unittest.TestCase):
+    """Test cases for the BaseModel class."""
+
+    def test_attributes(self):
+        """Test the public instance attributes."""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        self.assertEqual(type(my_model.id), str)
+        self.assertEqual(type(my_model.created_at), datetime)
+        self.assertEqual(type(my_model.updated_at), datetime)
+
+    def test_str_method(self):
+        """Test the __str__ method."""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        expected_str = "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__)
+        self.assertEqual(str(my_model), expected_str)
+
+    def test_save_method(self):
+        """Test the save method."""
+        my_model = BaseModel()
+        prev_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(prev_updated_at, my_model.updated_at)
+
+    def test_to_dict_method(self):
+        """Test the to_dict method."""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        self.assertEqual(type(my_model_json['created_at']), str)
+        self.assertEqual(type(my_model_json['updated_at']), str)
+        self.assertEqual(type(my_model_json['__class__']), str)
+        self.assertEqual(my_model_json['__class__'], 'BaseModel')
+
